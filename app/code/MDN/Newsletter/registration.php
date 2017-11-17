bacba6eeb6d4a7a2ddc8efc3226af8e97877164b
Renato <medinadato@gmail.com>
2017-11-17 21:18:07 +1100

    Fixed namespace

--- app/code/MDN/Newsletter/registration.php
+++ app/code/MDN/Newsletter/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Soul_Sample',
+    'MDN_Newsletter',
     __DIR__
 );
